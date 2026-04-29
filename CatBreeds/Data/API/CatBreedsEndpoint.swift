b259dfc8db697d44f5b7c4672841b7c990a48771
tiago.almeida <tiago.c.almeida@mindera.com>
2026-04-29 15:44:33 +0100

    [CB] - Add cat breeds remote data source

---
+++ CatBreeds/Data/API/CatBreedsEndpoint.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+enum CatBreedsEndpoint {
+    static func breeds(page: Int, limit: Int) -> Endpoint {
+        Endpoint(
+            path: "breeds",
+            method: .get,
+            queryItems: [
+                URLQueryItem(name: "limit", value: "\(limit)"),
+                URLQueryItem(name: "page", value: "\(page)")
+            ]
+        )
+    }
+}
---
+++ CatBreeds/Data/API/CatBreedsRemoteDataSource.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+protocol CatBreedsRemoteDataSource {
+    func fetchBreeds(page: Int, limit: Int) async throws -> BreedsPage
+}
+
+struct DefaultCatBreedsRemoteDataSource: CatBreedsRemoteDataSource {
+    private let apiClient: APIClient
+
+    init(apiClient: APIClient) {
+        self.apiClient = apiClient
+    }
+
+    func fetchBreeds(page: Int, limit: Int) async throws -> BreedsPage {
+        let endpoint = CatBreedsEndpoint.breeds(page: page, limit: limit)
+        let dtos: [CatBreedDTO] = try await apiClient.request(endpoint)
+
+        let breeds = dtos.map {
+            CatBreedMapper.map($0)
+        }
+
+        return BreedsPage(
+            breeds: breeds,
+            hasNextPage: dtos.count == limit
+        )
+    }
+}
