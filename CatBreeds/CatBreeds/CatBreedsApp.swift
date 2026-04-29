7fcd2a27c0990cc552b59a57ec4af55a9a6aaa20
tiago.almeida <tiago.c.almeida@mindera.com>
2026-04-29 14:17:21 +0100

    [CB] - Bootstrap Xcode project with SwiftUI and SwiftData

---
+++ CatBreeds/CatBreeds/CatBreedsApp.swift
@@ -0,0 +1,18 @@
+//
+//  CatBreedsApp.swift
+//  CatBreeds
+//
+//  Created by (Admin) Tiago Cunha Almeida on 29/04/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CatBreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
